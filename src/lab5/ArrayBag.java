6b8b6c074d71318782d37b22dae4d1bbfa5d5661
anthonywww <31651690+anthonywww@users.noreply.github.com>
2019-03-11 16:41:42 -0700

    Added Lab 5

---
+++ src/lab5/ArrayBag.java
@@ -0,0 +1,178 @@
+package lab5;
+
+import java.util.Arrays;
+
+/**
+ * A class of bags whose entries are stored in a fixed-size array.
+ * @param <T>
+ */
+public class ArrayBag<T> {
+
+	private T[] myArray;
+	private static final int DEFAULT_CAPACITY = 25;
+	private int mySize;
+
+	/** Creates an empty bag whose initial capacity is 25. */
+	public ArrayBag() {
+		this(DEFAULT_CAPACITY);
+	}
+
+	/**
+	 * Creates an empty bag having a given initial capacity.
+	 * 
+	 * @param capacity the integer capacity desired
+	 */
+	@SuppressWarnings("unchecked")
+	public ArrayBag(int capacity) {
+		myArray = (T[]) new Object[capacity];
+		mySize = 0;
+	}
+
+	/**
+	 * Adds a new entry to this bag.
+	 * 
+	 * @param newEntry the object to be added as a new entry
+	 * @return true if the addition is successful, or false if not
+	 */
+	public boolean add(T newEntry) {
+		if (!isFull()) {
+			myArray[mySize] = newEntry;
+			mySize++;
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Sees whether this bag is empty.
+	 * 
+	 * @return true if this bag is empty, or false if not
+	 */
+	public boolean isEmpty() {
+		return mySize == 0;
+	}
+
+	/**
+	 * Sees whether this bag is full.
+	 * 
+	 * @return true if this bag is full, or false if not
+	 */
+	public boolean isFull() {
+		return mySize == myArray.length;
+	}
+
+	/**
+	 * Gets the current number of objects in this bag.
+	 * 
+	 * @return the integer number of objects currently in the bag
+	 */
+	public int getCurrentSize() {
+		return mySize;
+	}
+
+	/**
+	 * Counts the number of times a given entry appears in this bag.
+	 * 
+	 * @param anEntry the entry to be counted
+	 * @return the number of times anEntry appears in the bag
+	 */
+	public int getFrequencyOf(Object anEntry) {
+		int freq = 0;
+
+		for (int i = 0; i < myArray.length; i++) {
+			if (anEntry.equals(myArray[i])) {
+				freq++;
+			}
+		}
+
+		return freq;
+	}
+
+	/**
+	 * Tests whether this bag contains a given entry.
+	 * 
+	 * @param anEntry the entry to locate
+	 * @return true if this bag contains anEntry, or false otherwise
+	 */
+	public boolean contains(Object anEntry) {
+
+		for (int i = 0; i < myArray.length; i++) {
+			if (anEntry.equals(myArray[i])) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Removes one unspecified entry from this bag, if possible.
+	 * 
+	 * @return either the removed entry, if the removal was successful, or null
+	 */
+	public Object remove() {
+		if (!isEmpty()) {
+			Object o = myArray[mySize - 1];
+			myArray[mySize - 1] = null;
+			mySize--;
+			return o;
+		}
+		return null;
+	}
+
+	/**
+	 * Removes one occurrence of a given entry from this bag.
+	 * 
+	 * @param anEntry the entry to be removed
+	 * @return true if the removal was successful, or false otherwise
+	 */
+	public boolean remove(Object anEntry) {
+		if (contains(anEntry)) {
+			for (int i = 0; i < mySize; i++) {
+				if (myArray[i].equals(anEntry)) {
+					myArray[i] = myArray[mySize];
+					myArray[mySize] = null;
+					mySize--;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/** Removes all entries from this bag. */
+	public void clear() {
+		for (int i = 0; i < myArray.length; i++) {
+			remove();
+		}
+	}
+
+	/**
+	 * Retrieves all entries that are in this bag.
+	 * 
+	 * @return a newly allocated array of all the entries in the bag
+	 */
+	public Object[] toArray() {
+		return Arrays.copyOf(myArray, mySize);
+	}
+
+	/**
+	 * Converts all the data in the bag into one big String
+	 * 
+	 * @return a very long String of objects contained in bag
+	 */
+	@Override
+	public String toString() {
+		int k;
+		String result = "[";
+		for (k = 0; k < mySize; k++) {
+			if (k < mySize - 1) {
+				result += myArray[k] + ", "; // up to item before last
+			} else {
+				result += myArray[k]; // the last item
+			}
+		}
+		return result + "]";
+	}
+
+}
---
+++ src/lab5/Lab5a.java
@@ -0,0 +1,161 @@
+package lab5;
+
+/** Lab 5 exercises -- Array based Bag, Stack, Queue and List Classes
+ *  
+ */
+public class Lab5a {
+	
+	public static void main(String[] args) {
+		
+        //********** Problem 0 Test constructors, isEmpty, isFull, getCurrentSize, toString *************
+        // declare 3 bags
+        System.out.println("**************** Problem 0 **************\n");
+        
+        ArrayBag duffleBag = new ArrayBag();
+        ArrayBag groceryBag = new ArrayBag(10);
+        ArrayBag tinyBag = new ArrayBag(0); // this bag can hold zero items
+        
+        System.out.println("   size of duffleBag = " + duffleBag.getCurrentSize());        
+        System.out.println("   is duffleBag empty? " + duffleBag.isEmpty());
+        System.out.println("   is duffleBag full? " + duffleBag.isFull());
+        System.out.println("contents of duffleBag: " + duffleBag);        
+        
+        System.out.println("\n   size of groceryBag = " + groceryBag.getCurrentSize());        
+        System.out.println("   is groceryBag empty? " + groceryBag.isEmpty());
+        System.out.println("   is groceryBag full? " + groceryBag.isFull());
+        System.out.println("contents of groceryBag: " + groceryBag);        
+        
+        System.out.println("\n   size of tinyBag = " + tinyBag.getCurrentSize());        
+        System.out.println("   is tinyBag empty? " + tinyBag.isEmpty());
+        System.out.println("   is tinyBag full? " + tinyBag.isFull());
+        System.out.println("contents of tinyBag: " + tinyBag);        
+        
+        
+        //********** End Problem 0  *****************************************************/        
+        //********** Problem 1 test your add method  ****************
+        System.out.println("\n\n**************** Problem 1 **************\n");
+        
+        // add some items
+        duffleBag.add("ball"); duffleBag.add("cap"); duffleBag.add("ball");
+        duffleBag.add("keys"); duffleBag.add("ball");
+        
+        System.out.println("   size of duffleBag = " + duffleBag.getCurrentSize());        
+        System.out.println("   is duffleBag empty? " + duffleBag.isEmpty());
+        System.out.println("   is duffleBag full? " + duffleBag.isFull());
+        System.out.println("contents of duffleBag: " + duffleBag);        
+
+        System.out.println();
+        
+        // check add returns boolean
+        if (groceryBag.add("candy bar"))
+            System.out.println("Successfully added one candy bar to groceryBag");
+            
+        // add until full    
+        while( groceryBag.add("candy bar"))
+            System.out.println("added another candy bar!");
+            
+        System.out.println("\n   size of groceryBag = " + groceryBag.getCurrentSize());        
+        System.out.println("   is groceryBag empty? " + groceryBag.isEmpty());
+        System.out.println("   is groceryBag full? " + groceryBag.isFull());
+        System.out.println("contents of groceryBag: " + groceryBag);        
+        
+        //********** End Problem 1  *****************************************************/  
+        //********** Problem 2 test your getFrequencyOf method  ****************        
+        System.out.println("\n\n**************** Problem 2**************\n");
+        
+        System.out.println("There are " + duffleBag.getFrequencyOf("ball") + " balls in duffleBag");
+        System.out.println("There are " + groceryBag.getFrequencyOf("candy bar") + " candy bars in groceryBag");
+        
+        
+        //********** End Problem 2  *****************************************************/
+        //********** Problem 3 test your contains method  *****************
+        System.out.println("\n\n**************** Problem 3**************\n");
+
+        System.out.println("Does duffleBag contain keys? " + duffleBag.contains("keys"));
+        System.out.println("Does groceryBag contain broccoli? " + groceryBag.contains("broccoli"));
+        
+        
+        //********** End Problem 3  *****************************************************/      
+        //********** Problem 4 Test the remove method ************** 
+        System.out.println("\n\n**************** Problem 4**************\n");
+        
+        System.out.println("Removing " + duffleBag.remove() + " from duffleBag");
+        System.out.println("   size of duffleBag = " + duffleBag.getCurrentSize());        
+        System.out.println("contents of duffleBag: " + duffleBag);        
+
+        System.out.println();
+
+        System.out.println("Removing " + tinyBag.remove() + " from tinyBag");
+        System.out.println("   size of tinyBag = " + tinyBag.getCurrentSize());        
+        System.out.println("contents of tinyBag: " + tinyBag);                    
+              
+        //********** End Problem 4  *****************************************************/     
+        //*************  Problem 5  Test the clear method ****************************
+        System.out.println("\n\n**************** Problem 5 **************\n");
+
+        groceryBag.clear();
+        
+        System.out.println("Clearing all items from groceryBag");
+        System.out.println("   size of groceryBag = " + groceryBag.getCurrentSize());        
+        System.out.println("   is groceryBag empty? " + groceryBag.isEmpty());
+        System.out.println("   is groceryBag full? " + groceryBag.isFull());
+        System.out.println("contents of groceryBag: " + groceryBag);        
+        
+        //********** End Problem 5  *****************************************************/     
+        //*************  Problem 6  (Optional) Test the remove(anEntry) method
+        System.out.println("\n\n**************** Problem 6 (Optional) **************\n");
+               
+        duffleBag.remove("keys");
+        
+        System.out.println("After remove(\"keys\"); from duffleBag");
+        System.out.println("   size of duffleBag = " + duffleBag.getCurrentSize());        
+        System.out.println("contents of duffleBag: " + duffleBag);        
+
+        System.out.println();   
+        
+        duffleBag.remove("book");
+        
+        System.out.println("After remove(\"book\"); from duffleBag");
+        System.out.println("   size of duffleBag = " + duffleBag.getCurrentSize());        
+        System.out.println("contents of duffleBag: " + duffleBag);        
+        
+        //********** End Problem 6  *****************************************************/   
+        //*************  Problem 7  (Optional) Test the remove(anEntry) method
+        System.out.println("\n\n**************** Problem 7 (Optional) **************\n");
+        
+        //Integer [] array = {10, 2, 4, 5};
+        //System.out.println("Can I add an array of Integer to duffleBag? " + duffleBag.add(array));
+        System.out.println("size of duffleBag = " + duffleBag.getCurrentSize());        
+        System.out.println("contents of duffleBag: " + duffleBag);        
+        
+        System.out.println("Follow the steps in Problem 7 to turn your ArrayBag into a\n" +
+                           " class of generic type T");
+        System.out.println("Then in step one add <String> after ArrayBag 6 times");
+        System.out.println("Recompile...you should see a compiler error preventing the previous add");
+        
+        
+        //********** End Problem 7  *****************************************************/   
+        //*************  Problem 8  (Optional) Test the toArray method
+        System.out.println("\n\n**************** Problem 8 (Optional) **************\n");
+        
+        Object [] contents = duffleBag.toArray();
+        
+        System.out.println("Here are the contents of duffleBag now in an array: ");
+        for (int k=0; k<contents.length; k++)
+            System.out.println(contents[k]);
+            
+        System.out.println("To use these items as String, you have to cast each back to String");
+        
+        String [] stringContents = new String[contents.length];
+        for (int k=0; k<contents.length; k++)
+            stringContents[k] = (String) contents[k];
+            
+        System.out.println("Here are the contents of duffleBag now in a String array: ");
+        for (int k=0; k<stringContents.length; k++)
+            System.out.println(stringContents[k].toUpperCase());        
+        
+        //********** End Problem 8  *****************************************************/   
+        
+        
+    }
+}
